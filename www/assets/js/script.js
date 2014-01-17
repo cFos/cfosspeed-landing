a0b09040b7d3347ae60f3254815fb1f8bc353f24
ArmorDarks <arkahor@gmail.com>
2014-01-17 21:15:23 +0200

    - [js] renamed reverse.js into script.js

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+
 	if ($(".landing-feature").css("margin-left") == "0px" ){
 		$($(".js-reverse").get().reverse()).each(function() {
 			$(this).appendTo($(this).parent());
 		});
 	}
+
 });
