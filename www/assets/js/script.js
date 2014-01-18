94f682c13deb9ef60ada83122d08cb736388d4ca
ArmorDarks <arkahor@gmail.com>
2014-01-18 20:02:33 +0200

    - [js] updated `reverseOrder` script

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
 
-	if ($(".landing-feature").css("margin-left") == "0px" ){
-		$($(".js-reverse").get().reverse()).each(function() {
-			$(this).appendTo($(this).parent());
-		});
-	}
+
+	function reverseOrder() {
+
+			if(!$('.js-reverse').hasClass('js-reversed')) {
+				if ($(".landing-feature").css("margin-left") == "0px" ){
+					$($(".js-reverse").get().reverse()).each(function() {
+						$(this).addClass('js-reversed');
+						$(this).appendTo($(this).parent());
+					});
+				};
+			};
+
+			if($('.js-reverse').hasClass('js-reversed')) {
+				if ($(".landing-feature").css("margin-left") != "0px" ){
+					$($(".js-reverse").get().reverse()).each(function() {
+						$(this).removeClass('js-reversed');
+						$(this).appendTo($(this).parent());
+					});
+				};
+			};
+
+	};
+
+
+	$(reverseOrder);
+
+
+	var resizeTimer;
+	$(window).resize(function() {
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(reverseOrder, 0);
+	});
+
 
 });
